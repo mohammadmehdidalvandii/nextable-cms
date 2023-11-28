44d0f3d31a7547f843fded0f1988dd163166cbbc
mohammadmehdi <mohammad333mehdi@gmail.com>
2023-11-28 20:45:33 +0330

    create grid page profile

--- src/page/Profile/Profile.jsx
+++ src/page/Profile/Profile.jsx
@@ -2,7 +2,22 @@
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <section className="profile">
+        <div className="row ">
+            <div className="col-12">
+                    this is col-12
+            </div>
+        </div>
+        <div className="row mt-5">
+            <div className="col-lg-8 col-md-12">
+                this is col 8
+            </div>
+            <div className="col-lg-4 col-md-12">
+                this is col 4
+            </div>
+        </div>
+        
+    </section>
   )
 }
 
